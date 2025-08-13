1aefd628eb97226f996241832e1af43f745015e8
kingvkay41-cyber <kingvkay41@gmail.com>
2025-08-13 13:13:30 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a= int(input("Enter The first number: "))
+b= int(input("Enter The second number: "))
+print("Addition :",a+b)
+print("Subtraction :",a-b)
+print("Multiplication :",a*b)
+print("Divison :",a/b)
+
+
+# In[ ]:
+
+
+
+
---
+++ Task 2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name=first_name+" "+last_name
+print("Hello,",full_name,"! Welcome to the the python program")
+
+
+# In[ ]:
+
+
+
+
